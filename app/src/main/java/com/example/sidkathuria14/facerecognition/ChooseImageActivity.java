6b444974ee8001ce7b95b6c1a21f3e5e4ac9183d
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-18 01:30:42 +0530

    adding ability to add images to scan

--- app/src/main/java/com/example/sidkathuria14/facerecognition/ChooseImageActivity.java
+++ app/src/main/java/com/example/sidkathuria14/facerecognition/ChooseImageActivity.java
@@ -2,22 +2,29 @@ package com.example.sidkathuria14.facerecognition;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class ChooseImageActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 234;
     private Uri filePath;
     ImageView imageView;
+    Bitmap bitmap;
+    public static final String TAG = "Face";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,19 @@ public class ChooseImageActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.detect)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ChooseImageActivity.this,MainActivity.class));
+//                Bitmap bmp = BitmapFactory.decodeFile (bitmap);
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Log.d(TAG, "onClick: " + byteArray.length);
+
+                Intent intent = new Intent(ChooseImageActivity.this,MainActivity.class);
+                intent.putExtra("picture",byteArray);
+                startActivity(intent);
             }
         });
 
@@ -54,7 +73,8 @@ public class ChooseImageActivity extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             filePath = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
                 imageView.setImageBitmap(bitmap);
 
             } catch (IOException e) {
--- app/src/main/java/com/example/sidkathuria14/facerecognition/MainActivity.java
+++ app/src/main/java/com/example/sidkathuria14/facerecognition/MainActivity.java
@@ -11,8 +11,10 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.gms.vision.face.Face;
 
@@ -24,18 +26,29 @@ import static com.example.sidkathuria14.facerecognition.R.id.imageView;
 public class MainActivity extends AppCompatActivity {
 
     private FaceOverlayView mFaceOverlayView;
-
+public static final String TAG1 = "tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFaceOverlayView = (FaceOverlayView) findViewById(R.id.face_overlay);
+        Log.d(TAG1, "onCreate: " + "main activity");
 
-        InputStream stream = getResources().openRawResource(R.raw.group);
-        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+//        InputStream stream = getResources().openRawResource(R.raw.group);
+//        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+//
+//        Bundle extras = getIntent().getExtras();
+//        byte[] byteArray = extras.getByteArray("picture");
+ byte[] byteArray = getIntent().getByteArrayExtra("picture");
 
-        mFaceOverlayView.setBitmap(bitmap);
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+
+     image.setImageBitmap(bmp);
+
+        mFaceOverlayView.setBitmap(bmp);
+        Log.d(TAG1, "onCreate: " + byteArray.length);
 
 
     }
